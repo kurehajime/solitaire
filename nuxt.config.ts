933b7c420cfe31b266cae21fcfec520d9bebe2c4
kurehajime <xiidec@gmail.com>
2024-07-13 14:26:18 +0900

    これいらないかも

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'static'
-  },
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
